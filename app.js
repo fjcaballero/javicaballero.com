9cd3f26a36e015959d5ae5ad74f12d3b9656933d
Javier <javichuck@hotmail.es>
2016-11-25 17:14:21 +0100

    Basic Server

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+//Public files
+app.use(express.static('public'));
+//node_modules
+app.use(express.static('node_modules'));
+//node_modules
+app.use(express.static('bower_components'));
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html');
+});
+
+app.listen(80);
+
+console.log('Servidor iniciado');
