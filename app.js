c9d34ffab2c947bb4a362e884e4af8ee47a44508
Javi Chuck <Javi Chuck>
2017-04-25 16:17:46 +0200

    Added 2DGame

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.set('port', (process.env.PORT || 8080));
 //Public files
 app.use(express.static('public'));
 app.use(express.static('public/images'));
+app.use(express.static('public/game-data'));
 //node_modules
 app.use(express.static('node_modules'));
 //node_modules
@@ -13,6 +14,10 @@ app.use(express.static('bower_components'));
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
+});
+
+app.get('/2dgame', function (req, res) {
+  res.sendFile('/public/games.html', { root: __dirname });
 });
 
 app.all('/*', function(req, res, next) {
